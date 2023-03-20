98c9f38e2731c5c1c1311d55a1e5b25b37553b11
devmegan <devmegan@protonmail.com>
2023-03-20 22:15:06 +0000

    add: get collection from mongo connection

--- app.py
+++ app.py
@@ -25,7 +25,20 @@ def mongo_connect(url):
         print("Could not connect to MongoDB: %s" % e)
 
 
-def show_memu():
+def get_collection(connection, db_name, collection_name):
+    if not connection:
+        print("Connection to MongoDB lost. Restart app.")
+        exit()
+    elif not db_name or not collection_name:
+        print("Make sure DB_NAME and COLL_NAME set in .env")
+        exit()
+
+    collection = connection[db_name][collection_name]
+    print("---\nCollection: " + str(collection) + "\n---")
+    return collection
+
+
+def show_menu():
     print("")
     print("1: Add a record")
     print("2: Find a record")
@@ -38,4 +51,6 @@ def show_memu():
 
 
 connection = mongo_connect(MONGO_URI)
-show_memu()
+collection = get_collection(connection, DB_NAME, COLLECTION_NAME)
+
+show_menu()
